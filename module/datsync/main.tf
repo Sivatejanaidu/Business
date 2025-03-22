078f9ebc6c71c1611226768f5785d7741de152d6
Sivatejanaidu <109169471+Sivatejanaidu@users.noreply.github.com>
2025-03-22 15:19:29 +0530

    Delete module/datsync directory

--- module/datsync/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_fsx_ontap_file_system" "testsingleazgen2" {
-  storage_capacity                = 4096
-  subnet_ids                      = [aws_subnet.test1.id]
-  deployment_type                 = "SINGLE_AZ_2"
-  ha_pairs                        = 4
-  throughput_capacity_per_ha_pair = 384
-  preferred_subnet_id             = aws_subnet.test1.id
-}
